667f72bd25504f880665be820c02c4ae0c7d9a3a
Daniel Faustino <danieloliveirafaustino@gmail.com>
2020-12-23 18:02:53 -0300

    conditions added to test if a repository exist

--- src/app.js
+++ src/app.js
@@ -59,6 +59,10 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id == id)
 
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+
   repositories.splice(repositoryIndex,1)
   return response.status(204).send()
 });
@@ -68,9 +72,13 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
- repositories[repositoryIndex].likes++
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
+  }
 
- return response.json(repositories[repositoryIndex])
+  repositories[repositoryIndex].likes++
+
+  return response.json(repositories[repositoryIndex])
 
 });
 
