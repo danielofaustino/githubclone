327438fa3ee0320c4b3f8baa44569da1b06108aa
Daniel Faustino <danieloliveirafaustino@gmail.com>
2020-12-23 17:29:41 -0300

    Put route created

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const repositories = [];
 app.get("/repositories", (request, response) => {
 
   return response.json(repositories)
-  
+
 });
 
 app.post("/repositories", (request, response) => {
@@ -32,7 +32,24 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const {title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id)
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found'})
+  }
+  
+  const repository = {
+    title:title,
+    url: url,
+    techs:techs
+  }
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
