44f2cf32d1668252be4ed897a53f8f377d277aa5
Daniel Faustino <danieloliveirafaustino@gmail.com>
2020-12-24 09:29:34 -0300

    New Screenshot

--- src/app.js
+++ src/app.js
@@ -30,9 +30,11 @@ function validateRepositoryId(request,response, next){
   
   const { id } = request.params;
 
-  if(!isUuid(id)) {
+  if(!isUuid(id)){
     return response.status(400).json({ error: 'Invalid Repository Id.'})
   }
+
+  return next();
 }
 
 app.use(logRequests);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 const app = require("./app");
 
-app.listen(4444,() => { console.log('Back-end Started 🚀')});
+app.listen(5555,() => { console.log('Back-end Started 🚀')});
